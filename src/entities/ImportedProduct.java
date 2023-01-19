57800cf1b304c1cac459f5802bc115a25b29faf8
EduardoLott <dudulott@gmail.com>
2023-01-19 02:02:53 -0300

    FirstCommit

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class ImportedProduct extends Product{
+    private double customsFee;
+
+    public ImportedProduct(){}
+    public ImportedProduct(String name, double price, double customsFee) {
+        super(name, price);
+        this.customsFee = customsFee;
+    }
+
+    public double getCustomsFee() {
+        return customsFee;
+    }
+
+    public void setCustomsFee(double customsFee) {
+        this.customsFee = customsFee;
+    }
+
+    public double totalPrice(){
+        return getPrice() + getCustomsFee();
+    }
+
+    @Override
+    public String priceTag(){
+        return getName()+ " $"+totalPrice()+ " (Customs fee: $"+getCustomsFee()+")";
+    }
+}
